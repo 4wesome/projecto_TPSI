2f52ad2dd7f0bd49a37fca08df40b11791de9ad4
4wesome <i10703@epcjc.net>
2014-11-04 14:58:26 +0000

    criação do ficheiro

---
+++ projecto_final/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+
+//este vai ser o menu principal, que será apresentado ao utilizador
+void menu_principal () {
+	
+int escolha;
+	
+printf("======================================\n");
+
+printf("bem vindo: \n");
+printf("escolha uma das seguintes opções: \n");
+printf("Gestao -> prima tecla 1 \n");
+printf("consultar base de dados -> prima tecla 2 \n");
+printf("alterar base de dados -> prima tecla 3 \n");
+printf("Sair -> prima tecla 4 \n");
+scanf("%d", &escolha);
+
+switch escolha {
+	case 1:
+		
+		
+} 
+
+	
+	
+}
+int main(int argc, char *argv[]) {
+	return 0;
+}
