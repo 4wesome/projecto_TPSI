565d02cd4e9f0b8ab5e87a4957e3dff0ec924db7
samuelc95 <i10717@epcjc.net>
2014-11-05 11:24:41 +0000

    Revert "ss3"
    This reverts commit 7a0b777a1822d96c0501878aa154fdc2a65e4759.

--- projecto_final/main.c
+++ projecto_final/main.c
@@ -4,58 +4,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void menu_gestao_consumos () { // este vai ser o menu principal, que será apresentado ao utilizador quando ele abre o programa
-	
-int escolha;
 
-do 
-{
-	system("cls");
-	printf("======================================\n");
-	printf("Menu Gestão de Consumos: \n");
-	printf("Escolha uma das seguintes opcoes: \n");
-	printf("Gestao clientes -> prima tecla 1 \n");
-	printf("Gestao consumos -> prima tecla 2 \n");
-	printf("consultar base de dados -> prima tecla 3 \n");
-	printf("alterar base de dados -> prima tecla 4 \n");
-	printf("Sair -> prima tecla 5 \n");
-	printf("======================================\n");
-	do {
-
-			scanf("%d", &escolha);
-		}
-	while (escolha<0 || escolha>4);
-	
-	switch (escolha) {
-	case 1:
-		system("cls");
-		//menu_gestao_clientes();
-		break;
-	case 2:
-		system("cls");
-		//menu_gestao_consumos();
-		break;
-	case 3:
-		system("cls");
-		//menu_consultas_bd();
-		break;
-	case 4:
-		system("cls");
-		//menu_ficheiros();
-		break;
-	default:
-		system("cls");
-		printf("o valor introduzido não é válido!");
-		break;
-	}
-}
-
-while (escolha != 5);
-
-
-	
-	
-}
 
 
 void menu_principal () { // este vai ser o menu principal, que será apresentado ao utilizador quando ele abre o programa
