2cff627c35de94cecb6801e5c04cca9a9c7d10b5
samuelc95 <i10717@epcjc.net>
2014-11-05 12:14:23 +0000

    Menus Prontos

--- projecto_final/main.c
+++ projecto_final/main.c
@@ -4,24 +4,223 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-<<<<<<< HEAD
-void menu_gestao_clientes() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+
+
+
+void menu_ficheiros() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
 	int escolha;
 
 do 
 {
 	system("cls");
 	printf("======================================\n");
-
-	printf("escolha uma das seguintes opcoes: \n");
-	printf("Novo -> prima tecla 1 \n");
-	printf("Editar -> prima tecla 2 \n");
-	printf("Remover -> prima tecla 3 \n");
-	printf("listar -> prima tecla 4 \n");
-	printf("voltar atras -> prima tecla 5 \n");
+	printf("Menu Consultas da Base de Dados\n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Ler Base de Dados \n");
+	printf("2 - Gravar Alteracoes \n");
+	printf("3 - Voltar Atras \n");
+	printf("\n");
 	printf("======================================\n");
 	do {
+			printf("Opcao escolhida: ");
+			scanf("%d", &escolha);
+		}
+	while (escolha<0 || escolha>3);
+	
+	switch (escolha) {
+	case 1:
+		system("cls");
+		//novo_cliente();
+		break;
+	case 2:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 3:
+		system("cls");
 
+		break;
+	default:
+		system("cls");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 3");
+		break;
+	}
+}
+
+while (escolha != 3);
+	
+}
+
+void menu_pesquisas() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+	int escolha;
+
+do 
+{
+	system("cls");
+	printf("======================================\n");
+	printf("Menu Consultas\n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Pesquisar Cliente por Codigo \n");
+	printf("2 - Pesquisar Cliente por Nome \n");
+	printf("3 - Pesquisar Consumo por Codigo \n");
+	printf("4 - Pesquisar Consumo por Codifo de Cliente \n");
+	printf("5 - Voltar Atras \n");
+	printf("\n");
+	printf("======================================\n");
+	do {
+			printf("Opcao escolhida: ");
+			scanf("%d", &escolha);
+		}
+	while (escolha<0 || escolha>5);
+	
+	switch (escolha) {
+	case 1:
+		system("cls");
+		//novo_cliente();
+		break;
+	case 2:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 3:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 4:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 5:
+		system("cls");
+
+		break;
+	default:
+		system("cls");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 5");
+		break;
+	}
+}
+
+while (escolha != 5);
+	
+}
+
+void menu_consultas() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+	int escolha;
+
+do 
+{
+	system("cls");
+	printf("======================================\n");
+	printf("Menu Consultas\n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Listar Clientes por Consumo \n");
+	printf("2 - Listar Clientes que nao pagaram \n");
+	printf("3 - Voltar Atras \n");
+	printf("\n");
+	printf("======================================\n");
+	do {
+			printf("Opcao escolhida: ");
+			scanf("%d", &escolha);
+		}
+	while (escolha<0 || escolha>3);
+	
+	switch (escolha) {
+	case 1:
+		system("cls");
+		//novo_cliente();
+		break;
+	case 2:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 3:
+		system("cls");
+
+		break;
+	default:
+		system("cls");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 3");
+		break;
+	}
+}
+
+while (escolha != 3);
+	
+}
+
+void menu_consultas_bd() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+	int escolha;
+
+do 
+{
+	system("cls");
+	printf("======================================\n");
+	printf("Menu Consultas da Base de Dados\n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Consulta \n");
+	printf("2 - Pesquisa \n");
+	printf("3 - Voltar Atras \n");
+	printf("\n");
+	printf("======================================\n");
+	do {
+			printf("Opcao escolhida: ");
+			scanf("%d", &escolha);
+		}
+	while (escolha<0 || escolha>3);
+	
+	switch (escolha) {
+	case 1:
+		system("cls");
+		menu_consultas();;
+		break;
+	case 2:
+		system("cls");
+		menu_pesquisas();
+		break;
+	case 3:
+		system("cls");
+
+		break;
+	default:
+		system("cls");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 3");
+		break;
+	}
+}
+
+while (escolha != 3);
+	
+}
+
+void menu_gestao_consumos() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+	int escolha;
+
+do 
+{
+	system("cls");
+	printf("======================================\n");
+	printf("Menu de Gestao de Consumos\n");
+	printf("\n");
+	printf("escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Novo \n");
+	printf("2 - Editar \n");
+	printf("3 - Remover \n");
+	printf("4 - Listar \n");
+	printf("5 - Voltar Atras \n");
+	printf("\n");
+	printf("======================================\n");
+	do {
+			printf("Opcao escolhida: ");
 			scanf("%d", &escolha);
 		}
 	while (escolha<0 || escolha>5);
@@ -45,10 +244,11 @@ do
 		break;
 	case 5:
 		system("cls");
-		menu_principal();
+		
+		break;		
 	default:
 		system("cls");
-		printf("o valor introduzido não é válido!");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 5");
 		break;
 	}
 }
@@ -56,10 +256,62 @@ do
 while (escolha != 5);
 	
 }
-=======
 
->>>>>>> 565d02cd4e9f0b8ab5e87a4957e3dff0ec924db7
+void menu_gestao_clientes() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os clientes (adicionar, editar, remover, e listar)
+	int escolha;
 
+do 
+{
+	system("cls");
+	printf("======================================\n");
+	printf("Menu de Gestao de Clientes\n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Novo \n");
+	printf("2 - Editar \n");
+	printf("3 - Remover \n");
+	printf("4 - Listar \n");
+	printf("5 - Voltar Atras \n");
+	printf("\n");
+	printf("======================================\n");
+	do {
+			printf("Opcao escolhida: ");
+			scanf("%d", &escolha);
+		}
+	while (escolha<0 || escolha>5);
+	
+	switch (escolha) {
+	case 1:
+		system("cls");
+		//novo_cliente();
+		break;
+	case 2:
+		system("cls");
+		//editar_cliente();
+		break;
+	case 3:
+		system("cls");
+		//remover_cliente();
+		break;
+	case 4:
+		system("cls");
+		//listar_cliente();
+		break;
+	case 5:
+		system("cls");
+		
+		break;	
+	default:
+		system("cls");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 5");
+		break;
+	}
+}
+
+while (escolha != 5);
+	
+}
 
 void menu_principal () { // este vai ser o menu principal, que será apresentado ao utilizador quando ele abre o programa
 	
@@ -69,19 +321,22 @@ do
 {
 	system("cls");
 	printf("======================================\n");
-	printf("bem vindo: \n");
-	printf("escolha uma das seguintes opcoes: \n");
-	printf("Gestao clientes -> prima tecla 1 \n");
-	printf("Gestao consumos -> prima tecla 2 \n");
-	printf("consultar base de dados -> prima tecla 3 \n");
-	printf("alterar base de dados -> prima tecla 4 \n");
-	printf("Sair -> prima tecla 5 \n");
+	printf("---------------Bem Vindo-------------- \n");
+	printf("\n");
+	printf("Escolha uma das seguintes opcoes: \n");
+	printf("\n");
+	printf("1 - Gestao clientes \n");
+	printf("2 - Gestao consumos  \n");
+	printf("3 - Consultar base de dados \n");
+	printf("4 - Alterar base de dados \n");
+	printf("5 - Sair \n");
+	printf("\n");
 	printf("======================================\n");
 	do {
-
+			printf("Opcao escolhida: ");
 			scanf("%d", &escolha);
 		}
-	while (escolha<0 || escolha>4);
+	while (escolha<0 || escolha>5);
 	
 	switch (escolha) {
 	case 1:
@@ -90,19 +345,23 @@ do
 		break;
 	case 2:
 		system("cls");
-		//menu_gestao_consumos();
+		menu_gestao_consumos();
 		break;
 	case 3:
 		system("cls");
-		//menu_consultas_bd();
+		menu_consultas_bd();
 		break;
 	case 4:
 		system("cls");
-		//menu_ficheiros();
+		menu_ficheiros();
+		break;
+	case 5:
+		system("cls");
+	
 		break;
 	default:
 		system("cls");
-		printf("o valor introduzido não é válido!");
+		printf("O Valor introduzido nao e valido! Devera introduzir um valor entre 1 e 5");
 		break;
 	}
 }
@@ -110,6 +369,7 @@ do
 while (escolha != 5);
 		
 }
+
 int main(int argc, char *argv[]) {
 	
 	menu_principal();
