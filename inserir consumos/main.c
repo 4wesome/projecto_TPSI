03bf6b275a503a7459c0c7cf4ebe39e54b27d9ba
samuelc95 <i10717@epcjc.net>
2014-11-11 14:31:14 +0000

    Update Projeto Final
    Inserçao de consumos adicionado ao ficheiro final

--- inserir consumos/main.c
+++ inserir consumos/main.c
@@ -31,7 +31,7 @@
 		int bi;
 		int num_fiscal;
 		consumo consumo[100];
-		int indice_consumo=0;
+		int indice_consumo;
 		int estado;
 	}cliente;
 	
@@ -44,9 +44,9 @@ int pesquisar_consumos(int codigo_consumo, int indiceCliente) {
 		      u = indice_cliente - 1;
 		      i = (l + u) / 2;
 		
-		      while ((l <= u) && (lista_clientes[indiceCliente].consumo[i].codigo != codigo_consumo))
+		      while ((l <= u) && (lista_clientes[indiceCliente].consumo[i].cod_consumo != codigo_consumo))
 		      {
-		            if(codigo_consumo < lista_clientes[indiceCliente].consumo[i].codigo)
+		            if(codigo_consumo < lista_clientes[indiceCliente].consumo[i].cod_consumo)
 		            {
 		                  u = i - 1;
 		            }
@@ -102,7 +102,7 @@ int pesquisar_clientes(int codigo_cliente) {
 
 void inserir_consumos(){
 
-	int op, i, a, cod_consumo, codigo_cliente;
+	int op, i, a, m, cod_consumo, codigo_cliente, existe = 0;
 
 	float preco_total;
 	
@@ -113,7 +113,7 @@ void inserir_consumos(){
           scanf("%d", &codigo_cliente);
           i = pesquisar_clientes(codigo_cliente);
 
-          if (lista_clientes[i].consumo[lista_clientes[i].indice_consumo].indice_consumo < 100)
+          if (lista_clientes[i].indice_consumo < 100)
           {
           
 			  if (i != -1) // Verifico se o código do cliente já existe
@@ -121,7 +121,7 @@ void inserir_consumos(){
 		         system("cls");
 		         printf("\n Introduza o codigo do consumo: ");
 		      	 scanf("%d", &cod_consumo);
-		      	 a = pesquisar_consumos(lista_clientes[i].indice_consumo, codigo_consumo);
+		      	 a = pesquisar_consumos(lista_clientes[i].indice_consumo, cod_consumo);
 		      	 
 		      	 if(lista_clientes[i].estado == 1){ //1 e ativo 0 e inativo
 		      	 		
@@ -135,7 +135,7 @@ void inserir_consumos(){
 		      
 						else // se o codigo do consumo ainda nao existir o utilizador pode inserir o resto dos campos
 						{
-						lista_clientes[i].consumo[lista_clientes[i].indice_consumo].cod_consumo = codigo_consumo;
+						lista_clientes[i].consumo[lista_clientes[i].indice_consumo].cod_consumo = cod_consumo;
 						
 						printf("\n Introduza o mes: ");
 						fflush(stdin);
@@ -146,13 +146,18 @@ void inserir_consumos(){
 						fflush(stdin);
 						scanf("%d", &lista_clientes[i].consumo[lista_clientes[i].indice_consumo].ano);
 						
+						existe = 0;
 						for(m=0; m <= lista_clientes[i].indice_consumo; m++)
 							{
-								if(lista_clientes[i].consumo[lista_clientes[i].indice_consumo].mes == lista_clientes[m].consumo[lista_clientes[m].indice_consumo].mes ){
+								if(lista_clientes[i].consumo[m].mes == lista_clientes[m].consumo[lista_clientes[m].indice_consumo].mes && lista_clientes[i].consumo[m].ano == lista_clientes[m].consumo[lista_clientes[m].indice_consumo].ano ){
 									
 									printf("\n Ja foi inserido um consumo para o mes e ano selecionados ");
+									existe = 1;
 								}
 							}
+							if(existe == 0)
+							{
+							
 						
 						printf("\n Introduza o consumo: ");
 						scanf("%f", &lista_clientes[i].consumo[lista_clientes[i].indice_consumo].consumo);
@@ -166,11 +171,12 @@ void inserir_consumos(){
 						printf("O preco total a pagar e de : %f \n", preco_total);
 						
 						// Incremento o número actual de consumo em +1
-						lista_clientes[i].consumo[lista_clientes[i].indice_consumo].indice_consumo++;
+						lista_clientes[i].indice_consumo++;
 						system("cls");
 						printf("\n O CONSUMO FOI ADICINADO COM SUCESSO.\n");
 						
 						}
+					}
 				   
 				   } else {
 				   		system("cls");
--- projecto_final/main.c
+++ projecto_final/main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define PRECO_GB 0.5
+#define IVA 0.23
+
 	typedef struct { // registo para a data
 		int dia;
 		int mes;
@@ -18,7 +21,7 @@
 		
 	}consumo;
 	
-	typedef struct { // registo para os clientes
+	typedef struct { // registo para os consumos
 		int codigo;
 		char nome[100];
 		char morada[250];
@@ -28,6 +31,7 @@
 		int bi;
 		int num_fiscal;
 		consumo consumo[100];
+		int indice_consumo;
 		int estado;
 	}cliente;
 	
@@ -35,8 +39,71 @@
 	int indice_cliente=0;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+//Funcoes declaradas no topo 
+int pesquisar_clientes(int codigo_cliente) {
+	int l,u,i;
+      l = 0;
+      u = indice_cliente - 1;
+      i = (l + u) / 2;
+
+      while ((l <= u) && (lista_clientes[i].codigo != codigo_cliente))
+      {
+            if(codigo_cliente < lista_clientes[i].codigo)
+            {
+                  u = i - 1;
+            }
+            else
+            {
+                  l = i + 1;
+            }
+            i = (l + u) / 2;
+      }
+
+      if(l <= u)
+      {
+            return i;
+      }
+      else
+      {
+            return -1;
+      }
+	
+	
+}
+
+int pesquisar_consumos(int codigo_consumo, int indiceCliente) {
+			int l,u,i;
+		      l = 0;
+		      u = indice_cliente - 1;
+		      i = (l + u) / 2;
+		
+		      while ((l <= u) && (lista_clientes[indiceCliente].consumo[i].cod_consumo != codigo_consumo))
+		      {
+		            if(codigo_consumo < lista_clientes[indiceCliente].consumo[i].cod_consumo)
+		            {
+		                  u = i - 1;
+		            }
+		            else
+		            {
+		                  l = i + 1;
+		            }
+		            i = (l + u) / 2;
+		      }
+		
+		      if(l <= u)
+		      {
+		            return i;
+		      }
+		      else
+		      {
+	            return -1;
+		      }
+			
+			
+}
 
 
+//fim funcoes
 
 void menu_ficheiros() {// este é o menu que será apresentado ao utilizador. aqui ele ira ler e gravar dados na base de dados
 	int escolha;
@@ -234,6 +301,105 @@ while (escolha != 3);
 
 
 
+
+void inserir_consumos(){
+
+	int op, i, a, m, cod_consumo, codigo_cliente, existe = 0;
+
+	float preco_total;
+	
+		  printf("_________________________________________________________________\n");
+          printf("\n     ADICIONAR Consumo");
+          printf("\n_________________________________________________________________\n");
+          printf("\n Introduza o codigo do cliente: ");
+          scanf("%d", &codigo_cliente);
+          i = pesquisar_clientes(codigo_cliente); //chama a funcao pesquisar clientes para varificar o codigo do cliente
+
+          if (lista_clientes[i].indice_consumo < 100) //so e permitido avancar se oliente tiver menos de 100 consumos
+          {
+          
+			  if (i != -1) // Verifico se o código do cliente já existe
+		      {
+		         system("cls");
+		         printf("\n Introduza o codigo do consumo: ");
+		      	 scanf("%d", &cod_consumo);
+		      	 a = pesquisar_consumos(lista_clientes[i].indice_consumo, cod_consumo);
+		      	 
+		      	 if(lista_clientes[i].estado == 1){ // 1 e ativo 0 e inativo
+		      	 		
+						if (a != -1) // Verifica-se se o código do consumo já existe
+						{
+						system("cls");
+						printf("\n O CODIGO DO CONSUMO JA EXISTE!!!\n\n Tente novamente.\n\n ");
+						system("pause");
+						system("cls");
+						}
+		      
+						else // se o codigo do consumo ainda nao existir o utilizador pode inserir o resto dos campos
+						{
+						lista_clientes[i].consumo[lista_clientes[i].indice_consumo].cod_consumo = cod_consumo;
+						
+						printf("\n Introduza o mes: ");
+						fflush(stdin);
+						scanf("%d", &lista_clientes[i].consumo[lista_clientes[i].indice_consumo].mes);
+						
+							
+						printf("\n Introduza o ano: ");
+						fflush(stdin);
+						scanf("%d", &lista_clientes[i].consumo[lista_clientes[i].indice_consumo].ano);
+						
+						existe = 0;
+						for(m=0; m <= lista_clientes[i].indice_consumo; m++) //verifica se o cliente ja tem um consumo adicionado opara o mes e ano pretendido
+							{
+								if(lista_clientes[i].consumo[m].mes == lista_clientes[m].consumo[lista_clientes[m].indice_consumo].mes && lista_clientes[i].consumo[m].ano == lista_clientes[m].consumo[lista_clientes[m].indice_consumo].ano ){
+									
+									printf("\n Ja foi inserido um consumo para o mes e ano selecionados \n");
+									existe = 1;
+								}
+							}
+							if(existe == 0)
+							{
+							
+						
+						printf("\n Introduza o consumo: ");
+						scanf("%f", &lista_clientes[i].consumo[lista_clientes[i].indice_consumo].consumo);
+						
+						//calcular o preco a pagar
+						preco_total = lista_clientes[i].consumo[lista_clientes[i].indice_consumo].consumo * PRECO_GB;
+						preco_total = preco_total + preco_total * IVA;
+						
+						lista_clientes[i].consumo[lista_clientes[i].indice_consumo].preco = preco_total;
+						
+						//preco a pagar e apresentado 
+						printf("O preco total a pagar e de : %f \n", preco_total);
+						
+						// Incremento o número actual de consumo em +1
+						lista_clientes[i].indice_consumo++;
+						system("cls");
+						printf("\n O CONSUMO FOI ADICINADO COM SUCESSO.\n");
+						
+						}
+					}
+				   
+				   } else {
+				   		system("cls");
+						printf("\n Nao podem ser adicionados consumos ao cliente caloteiro :D .\n");
+				   }
+		   	 					   
+		      } else {
+		      	printf("\n  ERRO!! O Codigo de Cliente nao existe \n");
+		      }
+          
+          } else {
+          		printf("\n  O cliente selecionado ja atingiu o limite de consumos \n");          	
+          }
+          
+          system("pause");
+
+          
+	
+}
+
             
             
 void menu_gestao_consumos() {// este é o menu que será apresentado ao utilizador. aqui ele ira puder gerir os consumos (adicionar, editar, remover, e listar)
@@ -263,7 +429,7 @@ do
 	switch (escolha) {
 	case 1:
 		system("cls");
-		//novo_consumo();
+		inserir_consumos();
 		break;
 	case 2:
 		system("cls");
@@ -292,36 +458,7 @@ while (escolha != 5);
 	
 }
 
-int pesquisar_clientes(int codigo_cliente) {
-	int l,u,i;
-      l = 0;
-      u = indice_cliente - 1;
-      i = (l + u) / 2;
 
-      while ((l <= u) && (lista_clientes[i].codigo != codigo_cliente))
-      {
-            if(codigo_cliente < lista_clientes[i].codigo)
-            {
-                  u = i - 1;
-            }
-            else
-            {
-                  l = i + 1;
-            }
-            i = (l + u) / 2;
-      }
-
-      if(l <= u)
-      {
-            return i;
-      }
-      else
-      {
-            return -1;
-      }
-	
-	
-}
 void inserir_clientes(){
 
 	int op, i, codigo_cliente;
