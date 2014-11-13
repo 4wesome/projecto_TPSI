2abbbbac29de3184ed852a0ce0e2365add4604ea
pintolindo <joaomiguel1994@hotmail.com>
2014-11-13 10:20:15 +0000

    pesquisa consumos
    erro em pesquisar

--- projecto_final/main.c
+++ projecto_final/main.c
@@ -272,6 +272,78 @@ while (escolha != 5);
 	
 }
 
+
+
+
+
+
+void consulta_consumos(){
+
+int x, i, n=1, soma_consumos; // soma_consumos é a soma de todos os consumos de um cliente
+float tab_cliente_totconsumo[30][2]; //tabela temporaria com os valores <codigo cliente> e <soma de consumos>
+float swap_consulta;
+float swap_cod_cliente[30][2];
+int h;
+for (x = 0;x <= indice_cliente; x++) // Esta funcao vai percorrer todos os clientes, desde 0 ate ao número actual de clientes na base de dados
+	{
+		
+	soma_consumos = 0;	
+		
+	tab_cliente_totconsumo[x][0] = (float)lista_clientes[x].codigo;
+	
+
+	for (i=0; i <= lista_clientes[x].indice_consumo; i++) // Esta funcao vai calcular a soma de todos os consumos de um cliente especifico (x)
+		{
+			for(h=0;h<=lista_clientes.indice_consumo;h++);
+			{
+			
+			soma_consumos = soma_consumos + lista_clientes[i].consumo[i];
+        }
+		}
+		
+	tab_cliente_totconsumo[x][1] = soma_consumos;
+	
+	} // no final deste FOR, o ARRAY tab_cliente_totconsumo[30][2] vai estar totalmente preenchido, com a primeira coluna mostrando o código de cliente e a segunda o somatório de todos os consumos.
+	
+	//////////////////////////////////////
+	
+	for(x = 0; x < indice_cliente - 2 ; x++) // função para ordenar os valores do ARRAY tab_cliente_totconsumo[30][2], de forma decrescente.
+		{
+			for(i = 0; i < indice_cliente - 2 - i; i--)
+				{		
+					if (tab_cliente_totconsumo[0][1] < tab_cliente_totconsumo[x+1][1])
+						{
+							swap_consulta = tab_cliente_totconsumo[0][1];
+							swap_cod_cliente = tab_cliente_totconsumo[0][0];
+							
+							tab_cliente_totconsumo[0][1] = tab_cliente_totconsumo[x+1][1];
+							tab_cliente_totconsumo[0][0] = tab_cliente_totconsumo[x+1][0];
+							
+							tab_cliente_totconsumo[x+1][1] = swap_consulta;
+							tab_cliente_totconsumo[x+1][0] = swap_cod_cliente;
+							
+							
+					}
+				}
+		}
+
+	//////////////////////////////
+
+	printf("Código Cliente | Consumos \n");
+	printf("\n");	
+
+		for (x = 0; x <= indice_clientes; x++) // Imprime tab_cliente_totconsumo - lista de clientes por consumo.
+		{
+				printf("%f | %f \n", tab_cliente_totconsumo[x][0],tab_cliente_totconsumo[x][1]);
+		}
+
+	
+}
+
+
+
+
+
 void menu_consultas() {// este é o menu que será apresentado ao utilizador. aqui consulta clientes por consumo e os que nao pagaram
 	int escolha;
 
@@ -297,7 +369,7 @@ do
 	switch (escolha) {
 	case 1:
 		system("cls");
-		//novo_cliente();
+		consulta_consumos();
 		break;
 	case 2:
 		system("cls");
