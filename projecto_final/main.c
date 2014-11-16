2b7dcfaae57f2ad1ec89a42750a57f7bf12ad980
pintolindo <joaomiguel1994@hotmail.com>
2014-11-16 19:57:09 +0000

    funcao procurar clientes que n pagaram
    ja nao tem erros ,vou estas agora

--- projecto_final/main.c
+++ projecto_final/main.c
@@ -373,7 +373,36 @@ for (x = 0;x <= indice_cliente; x++) // Esta funcao vai percorrer todos os clien
 }
 
 
+void consulta_inativos(){ // Esta função vai listar todos os clientes que tenham pelo menos 1 consumo não pago.
+int n = 1;
 
+do{
+
+	int x, i;
+	
+		// A função que se segue vai correr cada cliente, e, por cada um, vai analisar os seus consumos.
+		// Assim que identificar um consumo não pago, imprime uma msg e deixa de analisar mais consumos desse cliente, e passa para o próximo cliente.
+		for (x = 0; x < indice_cliente; x++) 
+		{
+		
+			for (i = 0; i < lista_clientes[x].indice_consumo; i++)
+			{
+				if (lista_clientes[x].consumo[i].estado == 0);
+					{
+						printf("%d  |  %s\n",lista_clientes[x].codigo, lista_clientes[x].nome);	
+						x++;
+						i = 0;
+					}
+			}
+		}
+		
+		printf("Prima 0 para voltar ao menu anterior.");
+		scanf("%", &n);
+	
+		
+}while (n != 0);
+
+}
 
 
 void menu_consultas() {// este é o menu que será apresentado ao utilizador. aqui consulta clientes por consumo e os que nao pagaram
@@ -405,7 +434,7 @@ do
 		break;
 	case 2:
 		system("cls");
-		//editar_cliente();
+		consulta_inativos();
 		break;
 	case 3:
 		system("cls");
