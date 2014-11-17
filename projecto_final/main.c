285d1af6880ac1e34f1a8213e264516fa996b1e4
pintolindo <joaomiguel1994@hotmail.com>
2014-11-17 15:04:20 +0000

    update

--- projecto_final/main.c
+++ projecto_final/main.c
@@ -479,11 +479,11 @@ for (x = 0;x <= indice_cliente; x++) // Esta funcao vai percorrer todos os clien
 
 void consulta_inativos(){ // Esta função vai listar todos os clientes que tenham pelo menos 1 consumo não pago.
 int n = 1;
-
-do{
-
-	int x, i;
+int x, i;
 	
+//do{
+
+
 		// A função que se segue vai correr cada cliente, e, por cada um, vai analisar os seus consumos.
 		// Assim que identificar um consumo não pago, imprime uma msg e deixa de analisar mais consumos desse cliente, e passa para o próximo cliente.
 		for (x = 0; x < indice_cliente; x++) 
@@ -491,20 +491,22 @@ do{
 		
 			for (i = 0; i < lista_clientes[x].indice_consumo; i++)
 			{
+                
 				if (lista_clientes[x].consumo[i].estado == 0);
 					{
+                                                        
 						printf("%d  |  %s\n",lista_clientes[x].codigo, lista_clientes[x].nome);	
 						x++;
-						i = 0;
+						
 					}
 			}
 		}
 		
-		printf("Prima 0 para voltar ao menu anterior.");
-		scanf("%", &n);
+	//	printf("Prima 0 para voltar ao menu anterior.");
+	//	scanf("%", &n);
 	
 		
-}while (n != 0);
+//}while (n != 0);
 
 }
 
